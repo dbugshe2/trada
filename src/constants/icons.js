07ad05885a17cf0337b19e2e37229bf9cd31ecdf
jonathan johnson <32993946+kxngnathan@users.noreply.github.com>
2020-04-08 02:50:42 +0100

    Assets and Assets code added

---
+++ src/constants/icons.js
@@ -0,0 +1,196 @@
+const icons = {
+  add: {
+    icon: require("../assets/icons/add.png"),
+    width: 17,
+    height: 17
+  },
+  back: {
+    icon: require("../assets/icons/back.png"),
+    width: 24,
+    height: 24
+  },
+  copy: {
+    icon: require("../assets/icons/copy.png"),
+    width: 12,
+    height: 12
+  },
+  event: {
+    icon: require("../assets/icons/event.png"),
+    width: 28,
+    height: 27
+  },
+  eventAlt: {
+    icon: require("../assets/icons/event-alt.png"),
+    width: 28,
+    height: 27
+  },
+  house: {
+    icon: require("../assets/icons/house.png"),
+    width: 30,
+    height: 30
+  },
+  houseAlt: {
+    icon: require("../assets/icons/house-alt.png"),
+    width: 30,
+    height: 30
+  },
+  recieved: {
+    icon: require("../assets/icons/recieved.png"),
+    width: 10,
+    height: 11
+  },
+  recievedAlt: {
+    icon: require("../assets/icons/recieved-alt.png"),
+    width: 10,
+    height: 11
+  },
+  sent: {
+    icon: require("../assets/icons/sent.png"),
+    width: 10,
+    height: 11
+  },
+  sentAlt: {
+    icon: require("../assets/icons/sent-alt.png"),
+    width: 10,
+    height: 11
+  },
+  storeFront: {
+    icon: require("../assets/icons/store-front.png"),
+    width: 27,
+    height: 27
+  },
+  storeFrontAlt: {
+    icon: require("../assets/icons/store-front-alt.png"),
+    width: 27,
+    height: 27
+  },
+  trend: {
+    icon: require("../assets/icons/trend.png"),
+    width: 27,
+    height: 20
+  },
+  trendAlt: {
+    icon: require("../assets/icons/trend-alt.png"),
+    width: 27,
+    height: 20
+  },
+  holdingGrain: {
+    icon: require('../assets/icons/holding-grain.png'),
+    width: 467,
+    height: 310,
+  },
+  logo: {
+    icon: require('../assets/icons/logo.png'),
+    width: 100,
+    height: 70,
+  },
+  plus: {
+    icon: require('../assets/icons/plus.png'),
+    width: 100,
+    height: 70,
+  },
+  logoAlt: {
+    icon: require('../assets/icons/logo-alt.png'),
+    width: 210,
+    height: 147,
+  },
+  cart: {
+    icon: require('../assets/icons/cart.png'),
+    width: 100,
+    height: 70,
+  }, 
+  basket: {
+    icon: require('../assets/icons/basket.png'),
+    width: 100,
+    height: 70,
+  },
+  market: {
+    icon: require('../assets/icons/market.png'),
+    width: 200,
+    height: 150,
+  },
+  avatar: {
+    icon: require('../assets/icons/avatar.png'),
+    width: 100,
+    height: 70,
+  },
+  trada: {
+    icon: require('../assets/icons/trada.png'),
+    width: 69,
+    height: 22,
+  },
+  
+  riceBag: {
+    icon: require('../assets/icons/ricebag.png'),
+    width: 30,
+    height: 60,
+  },
+  
+  coin: {
+    icon: require('../assets/icons/coin.png'),
+    width: 30,
+    height: 60,
+  },
+  support: {
+    icon: require("../assets/icons/support.png"),
+    width: 23,
+    height: 30
+  },
+  settings: {
+    icon: require("../assets/icons/settings.png"),
+    width: 23,
+    height: 30
+  },
+  logout: {
+    icon: require("../assets/icons/logout.png"),
+    width: 23,
+    height: 30
+  },
+  hcsw: {
+    icon: require("../assets/icons/logout.png"),
+    width: 23,
+    height: 30
+  },
+  farmer: {
+    icon: require("../assets/icons/farmer.png"),
+    width: 23,
+    height: 30
+  },
+  farmerAlt: {
+    icon: require("../assets/icons/farmer-alt.png"),
+    width: 23,
+    height: 30
+  },
+  commision: {
+    icon: require("../assets/icons/commision.png"),
+    width: 23,
+    height: 30
+  },
+  about: {
+    icon: require("../assets/icons/about.png"),
+    width: 23,
+    height: 30
+  },
+  menu: {
+    icon: require('../assets/icons/menu.png'),
+    width: 24,
+    height:24,
+  },
+  cashout: {
+    icon: require("../assets/icons/cashout.png"),
+    width: 30,
+    height: 31
+  },
+  cashoutAlt: {
+    icon: require("../assets/icons/cashout-alt.png"),
+    width: 30,
+    height: 30
+  },
+  lock: {
+    icon: require("../assets/icons/lock.png"),
+    width: 30,
+    height: 30
+  },
+};
+
+export default icons;
