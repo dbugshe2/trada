6245c8a3adce3a43bb5e595f53d52b0f1d25d6d8
jonathan johnson <32993946+kxngnathan@users.noreply.github.com>
2020-04-08 15:02:59 +0100

    Add constants and formatter

---
+++ src/constants/navigation.js
@@ -0,0 +1,8 @@
+import { theme } from '../utils';
+const { COLORS, SIZES } = theme;
+export const headerStyles = {};
+
+export const bottomBarStyles = {
+  backgroundColor: COLORS.background,
+  paddingBottom: SIZES.base,
+};
